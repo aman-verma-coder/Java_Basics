ad677d06154a82ccfd26d1fea78f5f889e7aff20
Aman Verma <9330nama@gmail.com>
2024-03-27 19:23:18 +0530

    Initial commit

---
+++ JavaBasics_9p2.java
@@ -0,0 +1,7 @@
+public class JavaBasics_9p2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");  // Ek line ka space
+        System.out.print("Hello World!\n");  // Ek line ka space
+        System.out.print("Hello World!");    // No next line space
+    }
+}
---
+++ variables_9p5.java
@@ -0,0 +1,12 @@
+public class variables_9p5 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        a = b;
+        b = 25;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
