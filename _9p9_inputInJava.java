1c6798d114b8d4e7106e9cf90235db5afaa52976
Aman Verma <9330nama@gmail.com>
2024-03-28 16:38:22 +0530

    9.9 input In Java.java

---
+++ _9p9_inputInJava.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class _9p9_inputInJava {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // String str = sc.next();
+        // System.out.println(str);
+        // String name = sc.nextLine();
+        // System.out.println(name);
+        // int num = sc.nextInt();
+        // System.out.println(num);
+        boolean ok = sc.nextBoolean();
+        System.out.println(ok);
+    }
+}
