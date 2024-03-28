e3d7d57515f6c8c2c4ea8c10ad530629954d25c1
Aman Verma <9330nama@gmail.com>
2024-03-28 21:59:25 +0530

    9p13 type Conversion in Java

---
+++ _9p13_typeConversion.java
@@ -0,0 +1,10 @@
+public class _9p13_typeConversion {
+    public static void main(String[] args) {
+        int a = 25;
+        long b = a;
+        System.out.println(a + " , " + b);
+        // long a = 25;
+        // int b = a;
+        // System.out.println(a + " , " + b);
+    }
+}
