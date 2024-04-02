e0a052310fc5d74eb8c993b4bc3b732f7ed86bbc
Aman Verma <9330nama@gmail.com>
2024-04-02 22:20:46 +0530

    10p4 Relational Operators in Java

---
+++ _10p4_Relational_Operators.java
@@ -0,0 +1,20 @@
+public class _10p4_Relational_Operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        int c = 10;
+        int d = 10;
+        System.out.println(c == d);
+        System.out.println(c != d);
+        System.out.println(c > d);
+        System.out.println(c < d);
+        System.out.println(c >= d);
+        System.out.println(c <= d);
+    }
+}
