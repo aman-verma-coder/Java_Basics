2c7cadd180ceba213b24446e4e1e15841b2c5785
Aman Verma <9330nama@gmail.com>
2024-04-02 21:46:48 +0530

    10p2 Arithmetic Operators in Java

---
+++ _10p2_Arithmetic_Operators.java
@@ -0,0 +1,11 @@
+public class _10p2_Arithmetic_Operators {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 15;
+        System.out.println("Sum= " + (a + b));
+        System.out.println("Diifference= " + (a - b));
+        System.out.println("Product= " + (a * b));
+        System.out.println("Division= " + (a / b));
+        System.out.println("Remainder= " + (a % b));
+    }
+}
