aaac2727e8309592cc3db518bb54cc280f40da22
Aman Verma <9330nama@gmail.com>
2024-04-02 22:10:07 +0530

    10p3 Unary Operators in Java

---
+++ _10p3_Unary_Operators.java
@@ -0,0 +1,26 @@
+public class _10p3_Unary_Operators {
+    public static void main(String[] args) {
+        // Increment
+        // PreIncrement
+        int a = 10;
+        int b = ++a;
+        System.out.println(a);
+        System.out.println(b);
+        // PostIncrement
+        int c = 10;
+        int d = c++;
+        System.out.println(c);
+        System.out.println(d);
+        // Decrement
+        // PreDecrement
+        int p = 10;
+        int q = --p;
+        System.out.println(p);
+        System.out.println(q);
+        // PostDecrement
+        int r = 10;
+        int s = r--;
+        System.out.println(r);
+        System.out.println(s);
+    }
+}
