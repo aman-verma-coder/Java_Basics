c5a2939863ada4b5183400587fb7792cb297e0a9
Aman Verma <9330nama@gmail.com>
2024-04-03 19:15:46 +0530

    11p2 Find largest between 2 numbers in Java

---
+++ _11p2_Find_largest_bw_2_nos.java
@@ -0,0 +1,13 @@
+public class _11p2_Find_largest_bw_2_nos {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 15;
+        if (a > b) {
+            System.out.println("A is greater than b");
+        } else if (a < b) {
+            System.out.println("B is greater than a");
+        } else {
+            System.out.println("Both are equal");
+        }
+    }
+}
