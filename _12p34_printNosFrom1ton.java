22025e9e3d4c9d421d2fe3976d50f038a8b29002
Aman Verma <9330nama@gmail.com>
2024-05-17 13:39:34 +0530

    _12p34_printNosFrom1ton.java

---
+++ _12p34_printNosFrom1ton.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class _12p34_printNosFrom1ton {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        int i = 1;
+        System.out.println("Printing numbers from 1 to " + n);
+        while (i <= n) {
+            System.out.println(i);
+            i++;
+        }
+        sc.close();
+    }
+}
