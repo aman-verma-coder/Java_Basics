fa9654a6faa67fbb1c0797bcbabbfcf5a500a48c
Aman Verma <9330nama@gmail.com>
2024-05-17 13:24:04 +0530

    _12p2_whileLoop.java

---
+++ _12p2_whileLoop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class _12p2_whileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 10) {
+            System.out.println("Hello World");
+            count++;
+        }
+    }
+}
