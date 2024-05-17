f2487236fdb3bfe9e90b3f6c11173857be4a865b
Aman Verma <9330nama@gmail.com>
2024-05-17 16:01:18 +0530

    _12p13_continue.java

---
+++ _12p13_continue.java
@@ -0,0 +1,10 @@
+public class _12p13_continue {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
