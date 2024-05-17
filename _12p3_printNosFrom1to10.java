439e198b2e8cd17d344c116d7b49d78b5c008545
Aman Verma <9330nama@gmail.com>
2024-05-17 13:32:01 +0530

    _12p3_printNosFrom1to10.java

---
+++ _12p3_printNosFrom1to10.java
@@ -0,0 +1,9 @@
+public class _12p3_printNosFrom1to10 {
+    public static void main(String[] args) {
+        int counter = 1;
+        while (counter <= 10) {
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
