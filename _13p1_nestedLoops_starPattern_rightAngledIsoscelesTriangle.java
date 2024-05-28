b1a184f10071bef939716bafded9141a69384356
Aman Verma <9330nama@gmail.com>
2024-05-28 16:03:21 +0530

    _13p1_nestedLoops_starPattern_rightAngledIsoscelesTriangle.java

---
+++ _13p1_nestedLoops_starPattern_rightAngledIsoscelesTriangle.java
@@ -0,0 +1,10 @@
+public class _13p1_nestedLoops_starPattern_rightAngledIsoscelesTriangle {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
